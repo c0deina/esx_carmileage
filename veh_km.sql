336753c58ec9a61c66a2d41c5e04e082739e8095
aNdrew <alex.alex959.aa@gmail.com>
2019-11-30 23:40:22 +0100

    Update veh_km.sql

--- veh_km.sql
+++ veh_km.sql
@@ -1,33 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 30-11-2019 a las 23:08:37
--- Versión del servidor: 10.1.38-MariaDB
--- Versión de PHP: 5.6.40
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `essentialmode`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `veh_km`
---
-
 CREATE TABLE `veh_km` (
   `carplate` varchar(10) NOT NULL,
   `km` varchar(255) NOT NULL DEFAULT '0',
@@ -35,19 +5,8 @@ CREATE TABLE `veh_km` (
   `reset` int(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `veh_km`
---
 ALTER TABLE `veh_km`
   ADD PRIMARY KEY (`carplate`),
   ADD UNIQUE KEY `carplate` (`carplate`),
   ADD KEY `carplate_2` (`carplate`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
